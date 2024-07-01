14cae6dab9f28c4074b4b72ed895363027438174
ChamudikaDeSilva <chamudikadesilva97@gmail.com>
2024-07-01 14:52:49 +0530

    Scrollable edit form in edit_product page

--- resources/js/Pages/Products/product_edit.jsx
+++ resources/js/Pages/Products/product_edit.jsx
@@ -118,7 +118,8 @@ export default function EditProduct() {
             <div className="py-12">
                 <div className="max-w-2xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 bg-white border-b border-gray-200">
+                    <div className="p-6 bg-white border-b border-gray-200 max-h-[calc(100vh-16rem)] overflow-y-auto">
+                        {/*<div className="p-6 bg-white border-b border-gray-200">*/}
                             <h2 className="font-semibold text-xl text-gray-800 leading-tight text-center mb-6">Edit Product</h2>
                             <form onSubmit={handleSubmit} className="flex flex-col items-center pb-20" encType="multipart/form-data">
                                 <div className="mt-4 w-full max-w-md">
