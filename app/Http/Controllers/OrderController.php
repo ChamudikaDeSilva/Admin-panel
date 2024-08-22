b105d7e4271da3716acc7e718efbb47dd39f5c56
ChamudikaDeSilva <chamudikadesilva97@gmail.com>
2024-08-22 16:53:42 +0530

    API For fetching orders

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -6,22 +6,25 @@ use App\Models\Discount;
 use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class OrderController extends Controller
 {
     public function getOrderCount()
     {
         $user = Auth::user();
+
         $orderCount = Order::where('user_id', $user->id)->count();
 
-        // Fetch the first two discounts
+        // Fetch the first two discounts and log them
         $firstOrderDiscount = Discount::where('code', 'disc001')->first();
         $fifthOrderDiscount = Discount::where('code', 'disc002')->first();
 
+        // Return the data as JSON
         return response()->json([
             'order_count' => $orderCount,
-            'first_order_discount' => $firstOrderDiscount->percentage ?? 0,
-            'fifth_order_discount' => $fifthOrderDiscount->percentage ?? 0,
+            'first_order_discount' => $firstOrderDiscount->value ?? 0,
+            'fifth_order_discount' => $fifthOrderDiscount->value ?? 0,
         ]);
     }
 
