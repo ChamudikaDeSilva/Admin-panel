c0eb8bc6c90de9debc08a80e1f42a0918c0436cf
ChamudikaDeSilva <chamudikadesilva97@gmail.com>
2024-12-10 11:10:49 +0530

    tt

--- resources/js/Pages/Products/deal_product_assign.jsx
+++ resources/js/Pages/Products/deal_product_assign.jsx
@@ -17,12 +17,12 @@ export default function AssignProducts() {
             <div className="py-12">
                 <div className="max-w-2xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <h2 className="font-semibold text-xl text-gray-800 leading-tight text-center mb-6 mt-6">Assign Products</h2>
+                        <h2 className="font-semibold text-xl text-gray-800 leading-tight text-center mb-6 mt-6">Assign Products</h2>
+
                             <form onSubmit="" className="flex flex-col items-center pb-20" encType="multipart/form-data">
-
-
-
                                 <div className="flex justify-center space-x-2 mt-4">
+
+                                    
                                     <Link
                                         href="/product/management/deals"
                                         className="px-4 py-2 bg-white-500 hover:bg-gray-200 text-gray-700 text-base rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-lime-500 sm:w-auto sm:text-sm"
