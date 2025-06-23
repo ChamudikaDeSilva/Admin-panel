008e720d990fab4a6f79184ce6e58c901dab87c8
ChamudikaDeSilva <chamudikadesilva97@gmail.com>
2025-06-23 10:58:02 +0530

    test

--- public/index.php
+++ public/index.php
@@ -1,17 +1,13 @@
 <?php
-// TEMPORARY DEBUG
-echo "Apache is serving index.php";
+
+echo "✅ Apache is serving Laravel public/index.php!";
 exit;
 
-// Laravel stuff (keep below)
+// Laravel bootstrap (leave this below)
 require __DIR__.'/../vendor/autoload.php';
-
 $app = require_once __DIR__.'/../bootstrap/app.php';
-
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 )->send();
-
 $kernel->terminate($request, $response);
