890bc2ec9d75d415d3ebcbb13dc0a53e76764995
ChamudikaDeSilva <chamudikadesilva97@gmail.com>
2025-06-24 11:12:35 +0530

    Refactor index.php for improved readability and organization of imports

--- public/index.php
+++ public/index.php
@@ -1,12 +1,18 @@
 <?php
 
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
 
+define('LARAVEL_START', microtime(true));
 
-// Laravel bootstrap (leave this below)
 require __DIR__.'/../vendor/autoload.php';
+
 $app = require_once __DIR__.'/../bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-)->send();
+
+$kernel = $app->make(Kernel::class);
+
+$response = tap($kernel->handle(
+    $request = Request::capture()
+))->send();
+
 $kernel->terminate($request, $response);
