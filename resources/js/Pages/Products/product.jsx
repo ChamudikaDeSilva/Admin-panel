53d250d0f209b74b237d089acf593cfc23574267
ChamudikaDeSilva <chamudikadesilva97@gmail.com>
2025-06-27 11:52:43 +0530

    Update product image source to use backend URL for consistency

--- resources/js/Pages/Products/product.jsx
+++ resources/js/Pages/Products/product.jsx
@@ -304,7 +304,7 @@ export default function Products({ auth }) {
                                                     </td>
                                                     <td className="px-6 py-4 whitespace-nowrap">
                                                         <img
-                                                            src={product.image} alt={product.name}
+                                                            src={`${import.meta.env.VITE_BACKEND_URL}/storage/${product.image}`} alt={product.name}
 
                                                             className="h-12 w-12 object-cover rounded"
                                                         />
