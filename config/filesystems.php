b11a0c1b85b163ee41c900d060e8ed798421f1e4
ChamudikaDeSilva <chamudikadesilva97@gmail.com>
2025-06-27 11:24:16 +0530

    Refactor filesystem configuration for improved clarity and organization

--- config/filesystems.php
+++ config/filesystems.php
@@ -69,8 +69,10 @@ return [
     |
     */
 
-    'links' => [
-        public_path('storage') => storage_path('app/public'),
-    ],
+    // 'links' => [
+    //     public_path('storage') => storage_path('app/public'),
+    // ],
+
+    'links' => [],
 
 ];
