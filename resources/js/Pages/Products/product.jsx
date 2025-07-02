16852547da863bb47c4b0453e58fdf7092346aa3
ChamudikaDeSilva <chamudikadesilva97@gmail.com>
2025-07-02 10:27:23 +0530

    Add symbolic link for images storage and clean up image rendering in product component

--- resources/js/Pages/Products/product.jsx
+++ resources/js/Pages/Products/product.jsx
@@ -305,11 +305,6 @@ export default function Products({ auth }) {
                                                     <td className="px-6 py-4 whitespace-nowrap">
                                                         <img
                                                             src={product.image} alt={product.name}
-
-
-
-
-
                                                             className="h-12 w-12 object-cover rounded"
                                                         />
                                                     </td>
